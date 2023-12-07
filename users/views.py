c363411e76994cd6dacf99ef54840156fc855893
Matthiiews <matthieu.kouassi@gmail.com>
2023-12-07 19:24:34 +0100

    Ajout du code dans le fichier users/views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from .serializers import SignupSerializer
+
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = SignupSerializer
 
 # Create your views here.
