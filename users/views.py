67db4715a3887c42f3aca6c6f0f6b76d69d93303
Matthiiews <matthieu.kouassi@gmail.com>
2023-12-09 14:50:39 +0100

    Refactorisation du users/views

--- users/views.py
+++ users/views.py
@@ -6,6 +6,9 @@ from .serializers import SignupSerializer
 
 
 class SignupView(generics.CreateAPIView):
+    """API view for user registration.
+    This view allows users to register by providing necessary information.
+    """
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = SignupSerializer
