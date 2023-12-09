c9d2db9840c5e2f631f2d27da544bd1c8d51e497
Matthiiews <matthieu.kouassi@gmail.com>
2023-12-09 15:46:43 +0100

    Refactorisation du fichier projects/urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -4,8 +4,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 
-rlpatterns = [
+urlpatterns = [
+    # URL pour la liste des projets
     path('', views.project_list),
+    # URL pour les détails d'un projet spécifique
     path('<int:project_pk>/', views.project_detail),
+    # URL pour la liste des contributeurs d'un projet spécifique
     path('<int:project_pk>/users/', views.contributor_list),
 ]
+
+# Ajout de format_suffix_patterns pour prendre en charge les suffixes de format
+urlpatterns = format_suffix_patterns(urlpatterns)
